2b95fe5bc5be8854f5be23e3beeb2ec5450bc4c5
Dhiraj <bharatkumar.jogdand@forgeahead.io>
2017-05-04 13:13:18 +0530

    new code

--- src/SampleCode.java
+++ src/SampleCode.java
@@ -2,8 +2,14 @@
  * Created by Ashok on 5/4/2017.
  */
 public class SampleCode {
+    public void m1()
+    {
+        System.out.println("method created by dhiraj");
+    }
 
     public static void main(String[] args) {
         System.out.println("Sample Application Runing Successfully");
+        SampleCode s=new SampleCode();
+        s.m1();
     }
 }
