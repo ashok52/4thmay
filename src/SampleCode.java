cfd6313a2f848875caaf623c4613c82489683194
ashokc <ashok.chavan@forgeahead.io>
2017-05-04 12:40:16 +0530

    updated on 4 may

---
+++ src/SampleCode.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ashok on 5/4/2017.
+ */
+public class SampleCode {
+
+    public static void main(String[] args) {
+        System.out.println("Sample Application Runing Successfully");
+    }
+}
